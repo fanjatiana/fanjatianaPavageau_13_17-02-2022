836c13b7791d351a62205e2f1b67917e5cba4530
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-02-22 11:54:51 +0100

    ajout de la page NotFound

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import PiggyBank from '../images/piggy_bank.png'
+import '../styles/main.css'
+const NotFound = () => {
+    /* author of this img : "http://www.freepik.com">*/
+    return (
+        <div className='error_404'>
+            <div className='block_img'>
+                <img
+                    src={PiggyBank}
+                    alt="tirelire cochon avec le texte oops page not found" />
+            </div>
+            <div className='block_link'>
+                <Link to="/" className='link_back-to-home'>
+                    Retour vers la page d'accueil
+                </Link>
+            </div>
+        </div >
+    );
+};
+
+export default NotFound;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import User from '../pages/User';
+import NotFound from '../pages/NotFound'
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path='/user' element={<User/>} />
+                <Route path='*' element={<NotFound/>} />
             </Routes>
         </>
     );
