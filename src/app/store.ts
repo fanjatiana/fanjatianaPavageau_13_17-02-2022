f341ab61979f9fab4b7534aeb1570e7c59c17f08
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-03-03 09:38:07 +0100

    part1 : page d' authentification avec redux et react toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import tokenReducer from '../features/tokenSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    token:tokenReducer, 
   },
 });
 
