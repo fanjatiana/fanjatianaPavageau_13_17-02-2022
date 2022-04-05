4e58deca80ffa049510010fee2ae536d5462431a
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-04-05 15:36:05 +0200

    modification du swagger : ajout des méthodes delete + post (validation soutenance)

--- src/services/postInfosLogin.ts
+++ src/services/postInfosLogin.ts
@@ -19,7 +19,6 @@ export const postInfoslogin = async (
   })
     .then((response) => response.json())
     .then((data) => {
-      //dispatch(getToken(data.body.token));
       localStorage.setItem("Bearer", data.body.token);
       dispatch(getInfoLoginStatus(true));
       navigate("/profile");
