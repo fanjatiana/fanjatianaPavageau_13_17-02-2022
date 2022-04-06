3644cab196d7d32ef5f9b4753ec40e358f41fb18
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-04-06 14:39:41 +0200

    modifs soutenance : gestion de l'accès à la page sign-in en fonction du token

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,18 +1,24 @@
 import '../styles/mediaQueries.css'
 import Login from '../components/Login';
+import { useAuth } from '../services/useAuth';
+import { Navigate } from 'react-router-dom';
 
 // login page
 
 const SignIn = () => {
-    return (
-        <main className="main bg-dark">
-            <section className="sign-in-content">
-                <i className="fa fa-user-circle sign-in-icon"></i>
-                <h2>Sign In</h2>
-                <Login />
-            </section>
-        </main>
-    );
-};
+    let auth: boolean = useAuth();
+    if (!auth) {
+        return (
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h2>Sign In</h2>
+                    <Login />
+                </section>
+            </main>
+        );
+    };
+    return <Navigate to="/profile" />;
+}
 
 export default SignIn;
