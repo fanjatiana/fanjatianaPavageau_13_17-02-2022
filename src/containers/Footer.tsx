a1432b81bf87d7955ec4a30deee88418280f82a2
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-04-06 15:02:17 +0200

    modifications css du footer (soutenance)

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -2,11 +2,11 @@
 
 const Footer = () => {
     return (
-        <div>
+        <>
             <footer className="footer">
                 <p className="footer-text">Copyright 2020 Argent Bank</p>
             </footer>
-        </div>
+        </>
     );
 };
 
